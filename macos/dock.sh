fc1e07a32f4e5f4cdbb29879a099c86e4ad7d5ba
Maji <steeven.regnault@pm.me>
2026-08-29 15:02:51 +0200

    chore: comment slack and ghostty

--- macos/dock.sh
+++ macos/dock.sh
@@ -13,7 +13,7 @@ add_to_dock() {
 }
 
 dockutil --no-restart --remove all
-add_to_dock "/Applications/Slack.app"
+# add_to_dock "/Applications/Slack.app"
 add_to_dock "/Applications/Discord.app"
 add_to_dock "/Applications/Telegram.app"
 add_to_dock "/Applications/Proton Pass.app"
@@ -21,8 +21,8 @@ add_to_dock "/Applications/Zen.app"
 add_to_dock "/System/Applications/Calendar.app"
 add_to_dock "/Applications/Obsidian.app"
 add_to_dock "/Applications/Visual Studio Code.app"
-add_to_dock "/Applications/Ghostty.app"
-# add_to_dock "/System/Applications/Utilities/Terminal.app"
+# add_to_dock "/Applications/Ghostty.app"
+add_to_dock "/System/Applications/Utilities/Terminal.app"
 add_to_dock "/System/Applications/System Settings.app"
 
 killall Dock >/dev/null 2>&1 || true
